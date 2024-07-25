f13c4e4ed1bed8eeb20fb03ece748288211c49e0
raymonepping <84765507+raymonepping@users.noreply.github.com>
2024-07-25 13:35:01 +0200

    Update build_environment.py

--- build_environment.py
+++ build_environment.py
@@ -8,17 +8,22 @@ import os
 import sys
 import logging
 import traceback
-
 from datetime import timedelta
 from dotenv import load_dotenv
-
 from couchbase.cluster import Cluster
 from couchbase.options import ClusterOptions, WaitUntilReadyOptions
 from couchbase.auth import PasswordAuthenticator
 from couchbase.diagnostics import ServiceType
-from couchbase.management.queries import CreatePrimaryQueryIndexOptions, CreateQueryIndexOptions
+from couchbase.management.queries import (
+    CreatePrimaryQueryIndexOptions,
+    CreateQueryIndexOptions,
+)
 from couchbase.management.search import SearchIndexManager
-from couchbase.management.buckets import CreateBucketSettings, BucketType, ConflictResolutionType
+from couchbase.management.buckets import (
+    CreateBucketSettings,
+    BucketType,
+    ConflictResolutionType,
+)
 from couchbase.management.collections import CreateCollectionSettings
 from couchbase.exceptions import (
     CouchbaseException,
@@ -26,10 +31,8 @@ from couchbase.exceptions import (
     ScopeAlreadyExistsException,
     BucketAlreadyExistsException,
     ScopeNotFoundException,
-    QueryIndexAlreadyExistsException
+    QueryIndexAlreadyExistsException,
 )
-
-# Import the couchbase module directly
 import couchbase
 
 # Load environment variables from .env file
@@ -37,17 +40,19 @@ load_dotenv()
 
 # Configure logging
 logging.basicConfig(
-    filename='app.log',
-    filemode='w',
+    filename="app.log",
+    filemode="w",
     level=logging.DEBUG,
-    format='%(levelname)s::%(asctime)s::%(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
+    format="%(levelname)s::%(asctime)s::%(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
 )
 
 # Add console logging
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
-console_handler.setFormatter(logging.Formatter('%(levelname)s::%(asctime)s::%(message)s'))
+console_handler.setFormatter(
+    logging.Formatter("%(levelname)s::%(asctime)s::%(message)s")
+)
 logger = logging.getLogger()
 logger.addHandler(console_handler)
 logger.setLevel(logging.DEBUG)
@@ -60,66 +65,88 @@ sdk_logger.setLevel(logging.ERROR)
 ops_threshold_logger = logging.getLogger("couchbase.operation_threshold")
 ops_threshold_logger.setLevel(logging.CRITICAL)
 
+
 # Add a filter to suppress specific warnings
 class SpecificWarningFilter(logging.Filter):
+    """
+    Filter to suppress specific warnings in the log output.
+    """
+
     def filter(self, record):
         if "Operations over threshold" in record.getMessage():
             return False
-        if "requested network \"auto\" is not found, fallback to \"default\" host" in record.getMessage():
+        if (
+            'requested network "auto" is not found, fallback to "default" host'
+            in record.getMessage()
+        ):
             return False
-        if "unable to find connected session with GCCCP support, retry in 2500ms" in record.getMessage():
+        if (
+            "unable to find connected session with GCCCP support, retry in 2500ms"
+            in record.getMessage()
+        ):
             return False
         return True
 
+    def add_to_logger(self, target_logger):
+        """
+        Add this filter to the specified logger.
+        """
+        target_logger.addFilter(self)
+
+
 logger.addFilter(SpecificWarningFilter())
 
-if hasattr(couchbase, 'configure_logging'):
+if hasattr(couchbase, "configure_logging"):
     couchbase.configure_logging(logger.name, level=logger.level)
 
 # Load configuration from config.json
-with open('config.json', encoding='utf-8') as config_file:
+with open("config.json", encoding="utf-8") as config_file:
     config = json.load(config_file)
 
-CREATE_BUCKET = config.get('createBucket', False)
-BUCKET_CONFIG = config.get('bucket', {})
-BUCKET_NAME = BUCKET_CONFIG.get('name')
-RAM_QUOTA_MB = BUCKET_CONFIG.get('ramQuotaMB', 100)
-FLUSH_ENABLED = BUCKET_CONFIG.get('flushEnabled', False)
-REPLICA_INDEX = BUCKET_CONFIG.get('replicaIndex', False)
-NUM_REPLICAS = BUCKET_CONFIG.get('numReplicas', 1)
-BUCKET_TYPE = BUCKET_CONFIG.get('bucketType', 'couchbase').upper()
-SCOPES = config.get('scopes', [])
-INDEXES = config.get('indexes', {})
+CREATE_BUCKET = config.get("createBucket", False)
+BUCKET_CONFIG = config.get("bucket", {})
+BUCKET_NAME = BUCKET_CONFIG.get("name")
+RAM_QUOTA_MB = BUCKET_CONFIG.get("ramQuotaMB", 100)
+FLUSH_ENABLED = BUCKET_CONFIG.get("flushEnabled", False)
+REPLICA_INDEX = BUCKET_CONFIG.get("replicaIndex", False)
+NUM_REPLICAS = BUCKET_CONFIG.get("numReplicas", 1)
+BUCKET_TYPE = BUCKET_CONFIG.get("bucketType", "couchbase").upper()
+SCOPES = config.get("scopes", [])
+INDEXES = config.get("indexes", {})
+
 
 def validate_config():
     """
     Validate the configuration loaded from config.json.
     """
     if not BUCKET_NAME:
-        logger.error('Bucket name must be specified in config.json.')
+        logger.error("Bucket name must be specified in config.json.")
         sys.exit(1)
     if not isinstance(SCOPES, list):
-        logger.error('Scopes must be a list in config.json.')
+        logger.error("Scopes must be a list in config.json.")
         sys.exit(1)
     if not isinstance(INDEXES, dict):
-        logger.error('Indexes must be a dictionary in config.json.')
+        logger.error("Indexes must be a dictionary in config.json.")
         sys.exit(1)
+
 
 def connect_to_cluster():
     """
     Connect to the Couchbase cluster and return the cluster object.
     """
-    auth = PasswordAuthenticator(os.getenv('COUCHBASE_USERNAME'), os.getenv('COUCHBASE_PASSWORD'))
+    auth = PasswordAuthenticator(
+        os.getenv("COUCHBASE_USERNAME"), os.getenv("COUCHBASE_PASSWORD")
+    )
     cluster = Cluster(
-        f'couchbase://{os.getenv("COUCHBASE_ENDPOINT")}', 
-        ClusterOptions(auth)
+        f'couchbase://{os.getenv("COUCHBASE_ENDPOINT")}', ClusterOptions(auth)
     )
     cluster.wait_until_ready(
-        timedelta(seconds=5), 
-        WaitUntilReadyOptions(service_types=[ServiceType.KeyValue, ServiceType.Query])
+        timedelta(seconds=5),
+        WaitUntilReadyOptions(service_types=[ServiceType.KeyValue, ServiceType.Query]),
     )
     logger.info("Successfully connected to Couchbase cluster.")
     return cluster
+
 
 def create_bucket(cluster):
     """
@@ -133,12 +160,15 @@ def create_bucket(cluster):
             ram_quota_mb=RAM_QUOTA_MB,
             num_replicas=NUM_REPLICAS,
             bucket_type=BucketType[BUCKET_TYPE],
-            conflict_resolution_type=ConflictResolutionType.SEQUENCE_NUMBER
+            conflict_resolution_type=ConflictResolutionType.SEQUENCE_NUMBER,
         )
         bucket_manager.create_bucket(bucket_settings)
-        logger.info('Bucket "%s" created with RAM quota %d MB.', BUCKET_NAME, RAM_QUOTA_MB)
+        logger.info(
+            'Bucket "%s" created with RAM quota %d MB.', BUCKET_NAME, RAM_QUOTA_MB
+        )
     except BucketAlreadyExistsException:
         logger.info('Bucket "%s" already exists.', BUCKET_NAME)
+
 
 def get_bucket(cluster):
     """
@@ -147,6 +177,7 @@ def get_bucket(cluster):
     bucket = cluster.bucket(BUCKET_NAME)
     logger.info('Successfully connected to bucket "%s".', BUCKET_NAME)
     return bucket
+
 
 def create_scope_and_collections(collection_manager, scope, collections):
     """
@@ -162,15 +193,18 @@ def create_scope_and_collections(collection_manager, scope, collections):
     for collection in collections:
         try:
             collection_manager.create_collection(
-                scope_name=scope, 
+                scope_name=scope,
                 collection_name=collection,
-                settings=CreateCollectionSettings()
+                settings=CreateCollectionSettings(),
             )
             logger.info('Collection "%s" created in scope "%s".', collection, scope)
         except CollectionAlreadyExistsException:
-            logger.info('Collection "%s" already exists in scope "%s".', collection, scope)
+            logger.info(
+                'Collection "%s" already exists in scope "%s".', collection, scope
+            )
         except ScopeNotFoundException:
             logger.error('The scope "%s" does not exist', scope)
+
 
 def create_indexes(cluster):
     """
@@ -184,56 +218,82 @@ def create_indexes(cluster):
             scope = primary.get("scope")
             collection = primary.get("collection")
             try:
-                logger.info('Creating primary index on collection "%s" in scope "%s".', collection, scope)
+                logger.info(
+                    'Creating primary index on collection "%s" in scope "%s".',
+                    collection,
+                    scope,
+                )
                 query_index_manager.create_primary_index(
                     BUCKET_NAME,
                     CreatePrimaryQueryIndexOptions(
                         scope_name=scope,
                         collection_name=collection,
-                        ignore_if_exists=True
-                    )
+                        ignore_if_exists=True,
+                    ),
                 )
                 logger.info(
-                    'Primary index created on collection "%s" in scope "%s".', 
-                    collection, scope
+                    'Primary index created on collection "%s" in scope "%s".',
+                    collection,
+                    scope,
                 )
             except ScopeNotFoundException:
-                logger.error('Scope "%s" not found for collection "%s".', scope, collection)
+                logger.error(
+                    'Scope "%s" not found for collection "%s".', scope, collection
+                )
             except CouchbaseException as e:
-                logger.error('Error creating primary index on collection "%s" in scope "%s": %s', collection, scope, e)
+                logger.error(
+                    'Error creating primary index on collection "%s" in scope "%s": %s',
+                    collection,
+                    scope,
+                    e,
+                )
 
     for index in INDEXES.get("secondary", []):
         if index.get("create", False):
-            index_name = index['name']
-            fields = index['fields']
+            index_name = index["name"]
+            fields = index["fields"]
             scope = index.get("scope")
             collection = index.get("collection")
             try:
-                logger.info('Creating secondary index "%s" on collection "%s" in scope "%s".', index_name, collection, scope)
+                logger.info(
+                    'Creating secondary index "%s" on collection "%s" in scope "%s".',
+                    index_name,
+                    collection,
+                    scope,
+                )
                 query_index_manager.create_index(
                     BUCKET_NAME,
                     index_name,
                     fields,
                     CreateQueryIndexOptions(
-                        scope_name=scope,
-                        collection_name=collection
-                    )
+                        scope_name=scope, collection_name=collection
+                    ),
                 )
                 logger.info(
-                    'Secondary index "%s" created on collection "%s" in scope "%s".', 
-                    index_name, collection, scope
+                    'Secondary index "%s" created on collection "%s" in scope "%s".',
+                    index_name,
+                    collection,
+                    scope,
                 )
             except QueryIndexAlreadyExistsException:
                 logger.info(
-                    'Secondary index "%s" already exists on collection "%s" in scope "%s".', 
-                    index_name, collection, scope
+                    'Secondary index "%s" already exists on collection "%s" in scope "%s".',
+                    index_name,
+                    collection,
+                    scope,
                 )
             except CouchbaseException as e:
-                logger.error('Error creating secondary index "%s" on collection "%s" in scope "%s": %s', index_name, collection, scope, e)
+                logger.error(
+                    'Error creating secondary index "%s" on collection "%s" in scope "%s": %s',
+                    index_name,
+                    collection,
+                    scope,
+                    e,
+                )
 
     for fts_index in INDEXES.get("fts", []):
         if fts_index.get("create", False):
-            fts_index_name = fts_index['name']
+            fts_index_name = fts_index["name"]
             scope = fts_index.get("scope")
             collection = fts_index.get("collection")
             fts_index_spec = {
@@ -241,32 +301,36 @@ def create_indexes(cluster):
                 "type": "fulltext-index",
                 "sourceName": BUCKET_NAME,
                 "sourceType": "couchbase",
-                "planParams": {
-                    "indexPartitions": 6
-                },
+                "planParams": {"indexPartitions": 6},
                 "params": {
                     "doc_config": {
-                        "mode": "scope.collection" if scope and collection else "bucket",
+                        "mode": (
+                            "scope.collection" if scope and collection else "bucket"
+                        ),
                         "scope_name": scope,
-                        "collection_name": collection
+                        "collection_name": collection,
                     },
-                    "mapping": {
-                        "default_mapping": {
-                            "enabled": True
-                        }
-                    }
-                }
+                    "mapping": {"default_mapping": {"enabled": True}},
+                },
             }
             try:
-                logger.info('Creating FTS index "%s" on collection "%s" in scope "%s".', fts_index_name, collection, scope)
+                logger.info(
+                    'Creating FTS index "%s" on collection "%s" in scope "%s".',
+                    fts_index_name,
+                    collection,
+                    scope,
+                )
                 search_index_manager.get_index(fts_index_name)
                 logger.info('FTS index "%s" already exists.', fts_index_name)
             except CouchbaseException:
                 search_index_manager.upsert_index(fts_index_spec)
                 logger.info(
-                    'FTS index "%s" created on collection "%s" in scope "%s".', 
-                    fts_index_name, collection, scope
+                    'FTS index "%s" created on collection "%s" in scope "%s".',
+                    fts_index_name,
+                    collection,
+                    scope,
                 )
+
 
 def main():
     """
@@ -283,13 +347,19 @@ def main():
 
         for scope_config in SCOPES:
             if scope_config.get("create", False):
-                create_scope_and_collections(collection_manager, scope_config["name"], scope_config["collections"])
+                create_scope_and_collections(
+                    collection_manager,
+                    scope_config["name"],
+                    scope_config["collections"],
+                )
 
         create_indexes(cluster)
 
-        logger.info('All bucket, scopes, collections, and indexes created successfully!')
+        logger.info(
+            "All bucket, scopes, collections, and indexes created successfully!"
+        )
     except CouchbaseException as err:
-        logger.error('ERR: %s', str(err))
+        logger.error("ERR: %s", str(err))
         logger.error(traceback.format_exc())
         sys.exit(1)
 
